21bb043047162417763ef0d3b3ad5c1fab4cdc0d
markhinojosa <markhinojosa1212@gmail.com>
2018-10-12 13:59:01 -0500

    added simulate scan button

--- screens/QRscreen.js
+++ screens/QRscreen.js
@@ -1,133 +1,12 @@
-// import React, { Component } from "react";
-// import { Text, View, StyleSheet, Alert } from "react-native";
-// import { Constants, BarCodeScanner, Permissions } from "expo";
-// import { Button } from "react-native";
-// import { connect } from "react-redux";
-// import { getQRData } from "../actions/AssetActions";
-
-// //to test QR functionality without a camera do the following:
-// // * comment out lines 40, and 78 - 87
-// // * uncomment lines 42-53, and 88-95
-
-// class QRCapture extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       hasCameraPermission: null
-//     };
-//   }
-
-//   componentDidMount() {
-//     this._requestCameraPermission();
-//   }
-
-//   _requestCameraPermission = async () => {
-//     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-//     this.setState({
-//       hasCameraPermission: status === "granted"
-//     });
-//   };
-
-//   handleQRForward = () => {
-//     // let checkForTimestamp = Object.keys(this.state).map((propName, idx) => {
-//     //   if(propName === "timestamp"){
-//     //     let dTime = transDat.dTime;
-//     //     this.setState()
-//     //   }
-//     // })
-
-//     const passStateToRedux = this.props.getQRData(this.state);
-
-//     const { navigate } = this.props.navigation;
-//     navigate("InputMan");
-//   };
-
-//   _handleBarCodeRead = data => {
-//     const scanResult = data.data;
-//     const splitScanResult = scanResult.split(", ");
-
-//     // const splitScanResult = [
-//     //   "0.9999 AU",
-//     //   "AGLD-0002",
-//     //   "072169",
-//     //   "timestamp",
-//     //   "LBMA",
-//     //   "AmagiMetals",
-//     //   "A-2",
-//     //   "1kg"
-//     // ];
-
-//     splitScanResult.map((value, idx) => {
-//       let timestamp = new Date().toLocaleString();
-
-//       if (value === "timestamp") {
-//         splitScanResult.splice(idx, 1, timestamp);
-//       }
-//     });
-
-//     this.setState(
-//       {
-//         CorePropValues: splitScanResult
-//       },
-//       () => this.handleQRForward()
-//     );
-//   };
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         {this.state.hasCameraPermission === null ? (
-//           <Text>Requesting for camera permission</Text>
-//         ) : this.state.hasCameraPermission === false ? (
-//           <Text>Camera permission is not granted</Text>
-//         ) : (
-//           <BarCodeScanner
-//             onBarCodeRead={this._handleBarCodeRead}
-//             style={{ height: 200, width: 200 }}
-//           />
-//         )}
-//         {/* <Button
-//           onPress={() => {
-//             this._handleBarCodeRead();
-//           }}
-//           title="click to simulate scan"
-//           color="#841584"
-//           accessibilityLabel="click to simulate scan"
-//         /> */}
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//     paddingTop: Constants.statusBarHeight,
-//     backgroundColor: "#ecf0f1"
-//   }
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   getQRData: data => dispatch(getQRData(data))
-// });
-
-// export default connect(
-//   null,
-//   mapDispatchToProps
-// )(QRCapture);
-
-
-
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
-import { Constants, BarCodeScanner, Permissions } from "expo";
+import { ScrollView, StyleSheet, View, Text, Button } from 'react-native';
+import { Constants, BarCodeScanner, Permissions, FileSystem } from "expo";
 export default class LinksScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hasCameraPermission: null
+            hasCameraPermission: null,
+            url: ""
         };
     }
     componentDidMount() {
@@ -144,10 +23,35 @@ export default class LinksScreen extends React.Component {
         });
     };
 
+    _handleBarCodeRead = payload => {
+        console.log("read QR")
+        // const scanResult = payload.data;
+        // this.setState(
+        //     {
+        //         url: scanResult
+        //     }, () => this._downloadFromURL()
+        // )
+        this._downloadFromURL()
+    };
+
+    _downloadFromURL = remoteUrl => {
+        console.log("running download from url")
+        FileSystem.downloadAsync(
+            'https://firebasestorage.googleapis.com/v0/b/doc-qr-share.appspot.com/o/documents%2FtestText.txt?alt=media&token=3b2d8653-79f6-4f36-afed-2aa69de2b597',
+            FileSystem.documentDirectory + 'thisIsAFolder' + 'testText.txt'
+        )
+            .then(({ uri }) => {
+                console.log('Finished downloading to ', uri);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                {this.state.hasCameraPermission === null ? (
+                {/* {this.state.hasCameraPermission === null ? (
                     <Text>Requesting for camera permission</Text>
                 ) : this.state.hasCameraPermission === false ? (
                     <Text>Camera permission is not granted</Text>
@@ -156,15 +60,15 @@ export default class LinksScreen extends React.Component {
                                 onBarCodeRead={this._handleBarCodeRead}
                                 style={{ height: 200, width: 200 }}
                             />
-                        )}
-                {/* <Button
+                        )} */}
+                <Button
           onPress={() => {
             this._handleBarCodeRead();
           }}
           title="click to simulate scan"
           color="#841584"
           accessibilityLabel="click to simulate scan"
-        /> */}
+        />
             </View>
         );
     }
